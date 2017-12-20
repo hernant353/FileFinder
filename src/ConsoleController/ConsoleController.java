c3475278bc8a44257c02a801b5a70d5fa10c2542
Christopher Dani <christopherdani@hotmail.com>
2017-12-19 21:12:35 -0500

    Added Exception catching in case file does not exist Added basic program run time measurements

--- src/ConsoleController/ConsoleController.java
+++ src/ConsoleController/ConsoleController.java
@@ -9,12 +9,28 @@ public class ConsoleController {
 
 
     public static void main(String[] args) throws IOException {
+        // Start time
+        long start = System.currentTimeMillis();
+
         SystemManager sm = new SystemManager();
         sm.addDirectory("C:\\Users");
         ArrayList<UserFile> ufiles = sm.getOldUserFiles();
+
         for (UserFile file: ufiles){
-            System.out.println(file.getFileName() + "     " + file.getLastAccessedDate());
+            try {
+                System.out.println(file.getFileName() + "     " + file.getLastAccessedDate());
+            }
+            catch (Exception e){
+                System.out.println("File: " + file + " may not exist!");
+                continue;
+            }
         }
+
+        // Stop time
+        long stop = System.currentTimeMillis();
+        System.out.println("\n\n The program ran for "+
+                (((stop - start) / 1000) / 60) + " minutes, and " +
+                (((stop - start) / 1000) % 60) + " seconds.");
     }
 
 
